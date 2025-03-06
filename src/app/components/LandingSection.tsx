b4b12a43575790199696d629e1610ac33bf0e614
Nicholas Liu <bz86nick@gmail.com>
2025-03-05 20:43:45 -0800

    Update library versions and migrate to MUI styling

--- src/app/components/LandingSection.tsx
+++ src/app/components/LandingSection.tsx
@@ -1,3 +1,4 @@
+"use client";
 /* eslint-disable react/react-in-jsx-scope */
 import {
   Avatar,
@@ -7,113 +8,159 @@ import {
   CardContent,
   CardMedia,
   Container,
-  Grid,
+  Grid2 as Grid,
   Paper,
   Typography,
 } from "@mui/material";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import Link from "next/link";
-import styles from "./landingSection.module.css";
+import contentEN from "@/app/locale/en/home.json";
+import { useThemeContext } from "@/app/contexts/themeContext";
 
 const LandingSection: React.FC = () => {
+  const { mode } = useThemeContext();
+  const content = contentEN;
+
   return (
     <Container
-      maxWidth={false}
-      className={styles.container}
-      sx={{ display: "flex", flexDirection: { xs: "column", md: "row" } }}
+      maxWidth={"xl"}
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", lg: "row" },
+        backgroundColor: mode === "light" ? "#F8F9FA" : "#121212",
+        width: "100%",
+        py: 8,
+        px: { sm: 8 },
+        gap: 10,
+      }}
     >
+      {/* About Card */}
       <Paper
         variant="elevation"
-        className={styles.profileCard}
-        sx={{ width: { xs: "100%", md: "40%" } }}
+        sx={{
+          width: { xs: "100%", lg:"80%" },
+          minWidth: { xs: "300px" },
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: mode === "light" ? "#2D2D2D" : "#E0E0E0",
+          backgroundColor: mode === "light" ? "#F8F9FA" : "#121212",
+          transform: "translateY(0)",
+          transition: "transform 200ms ease-in-out",
+          "&:hover": {
+            transform: "translateY(-10px)",
+            boxShadow: 6,
+          },
+          background:
+            mode === "light"
+              ? "rgba(255, 255, 255, 0.7)"
+              : "rgba(30, 30, 30, 0.7)",
+          backdropFilter: "blur(12px)",
+          border: "1px solid rgba(255, 255, 255, 0.3)",
+          boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1)",
+          borderRadius: "0.375rem",
+        }}
       >
         <Avatar
-          src="/images/avatar.jpeg"
-          alt="Guanming Liu"
+          src={content.about.imageSrc}
+          alt={content.about.imageAlt}
           sx={{ width: 100, height: 100, mb: 2 }}
         />
-        <Typography variant="h5" className={styles.profileTitle}>
-          About Me
+        <Typography variant="h5" sx={{ fontWeight: "bold", mb: 2 }}>
+          {content.about.title}
         </Typography>
-        <Typography className={styles.profileParagraph}>
-          My name is Guanming (Nicholas) Liu and I am a Software/Web Developer
-          based in Vancouver, BC, Canada. You can find my work on this portfolio
-          website.
+        <Typography
+          sx={{ lineHeight: 1.625, overflowWrap: "anywhere", hyphens: "auto" }}
+        >
+          {content.about.body}
         </Typography>
       </Paper>
-      <Grid container spacing={0}>
-        <Grid item xs={12} sx={{ textAlign: "center", height: 40 }}>
-          <Typography fontSize={30} className={styles.workTitle}>
-            My Work
+
+      {/* Work Display */}
+      <Grid container spacing={4} sx={{ marginBottom: 4 }}>
+        <Grid size={{ xs: 12 }} sx={{ textAlign: "center", height: 40 }}>
+          <Typography
+            sx={{
+              fontSize: 30,
+              color: mode === "light" ? "#2D2D2D" : "#E0E0E0",
+              background: mode === "light" ? "#8B5CF6" : "#7c3aed",
+              border: "1px solid gray",
+              borderRadius: "0.375rem",
+              boxShadow: 2,
+            }}
+          >
+            {content.projects.title}
           </Typography>
         </Grid>
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={6}>
-            <Card variant="outlined" className={styles.workCard}>
+        {content.projects.list.map((project, index) => (
+          <Grid size={{ xs: 12, md: 6 }} key={index}>
+            <Card
+              variant="outlined"
+              sx={{
+                padding: 2,
+                borderRadius: "0.375rem",
+                transition: "transform 300ms cubic-bezier(0.22, 1, 0.36, 1)",
+                "&:hover": {
+                  scale: 1.03,
+                  boxShadow: 8,
+                },
+                color: mode === "light" ? "#2D2D2D" : "#E0E0E0",
+                background:
+                  mode === "light"
+                    ? "rgba(255, 255, 255, 0.7)"
+                    : "rgba(30, 30, 30, 0.7)",
+                backdropFilter: "blur(12px)",
+                border: "1px solid rgba(255, 255, 255, 0.3)",
+                boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1)",
+              }}
+            >
               <CardMedia
                 component="img"
-                src="/promatch.png"
-                title="Promatch landing page"
-                className={styles.workCardImage}
+                src={project.imageSrc}
+                title={project.title}
+                sx={{ height: 200, objectFit: "contain" }}
               />
               <CardContent>
-                <Typography variant="h6" className={styles.workCardTitle}>
-                  ProMatch Marketing Page
-                </Typography>
-                <Typography className={styles.workCardParagraph}>
-                  The marketing landing page for an AI recruiting company
-                  ProMatch. Built features such as localization, language switch
-                  and Blog Posts using Content Management Systems (CMS) from
-                  Sanity.
+                <Typography variant="h6">{project.title}</Typography>
+                <Typography
+                  sx={{
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 4, // lines limit
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    height: "6em",
+                  }}
+                >
+                  {project.description}
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size="medium">
+                <Button
+                  size="medium"
+                  sx={{
+                    backgroundColor: mode === "light" ? "#2563EB" : "#3B82F6",
+                    "&:hover": {
+                      backgroundColor: mode === "light" ? "#3B82F6" : "#2563EB",
+                    },
+                  }}
+                >
                   <Link
-                    href="https://promatch.ai"
+                    href={project.link}
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="text-white"
                   >
-                    View Website
+                    {project.displayAction}
                     <OpenInNewIcon sx={{ ml: 1 }} />
                   </Link>
                 </Button>
               </CardActions>
             </Card>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Card variant="outlined" className={styles.workCard}>
-              <CardMedia
-                component="img"
-                src="/promatch.png"
-                title="Promatch"
-                className={styles.workCardImage}
-              />
-              <CardContent>
-                <Typography variant="h6" className={styles.workCardTitle}>
-                  ProMatch (Chinese Site) 中文站
-                </Typography>
-                <Typography className={styles.workCardParagraph}>
-                  The next-gen AI-driven Application Tracking System (ATS).
-                  Offering a highly customizable AI automated hiring solution
-                  that filters through candidates.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="medium">
-                  <Link
-                    href="https://promatch-ai.hackhub.cn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    View Website (Chinese)
-                    <OpenInNewIcon sx={{ ml: 1 }} />
-                  </Link>
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        </Grid>
+        ))}
       </Grid>
     </Container>
   );
